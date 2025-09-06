1ca6a700a2ab5648d211d2402eb5115d9e641fa9
Heather Lane <heather@illation.net>
2025-09-06 12:16:43 -0400

    MCP connection and basic tool call in place.

--- src/wikisearch_agent/main.py
+++ src/wikisearch_agent/main.py
@@ -1,11 +1,19 @@
 """Core wikisearch agent application infra."""
 
 from typing import Optional
+import asyncio
 from wikisearch_agent.util import fetch_api_keys, setup_logging
 from dataclasses import asdict
 import langsmith
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import get_buffer_string
+from langchain_core.output_parsers import JsonOutputParser, JsonOutputToolsParser, StrOutputParser
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
+from pathlib import Path
 
 
 class App:
@@ -17,7 +25,7 @@ class App:
     def __init__(self, argv: Optional[list[str]] = None) -> None:
         # TODO(heather) Handle command-line, if necessary. For the moment, assume
         # logging defaults are good.
-        self.logger = setup_logging()
+        self.logger = setup_logging(loglevel='INFO')
         self.secrets = fetch_api_keys()
         if self.secrets.openai_api is None:
             raise PermissionError("Don't have access to OpenAI API")
@@ -25,24 +33,45 @@ class App:
         self.tracing_client = langsmith.Client(api_key=self.secrets.langsmith_api)
         # TODO(heather) Consider factoring out the model name and temperature to command line args.
         self.llm = ChatOpenAI(model='gpt-5-mini', api_key=self.secrets.openai_api) # type: ignore
+        self.w_mcp_sever_params = StdioServerParameters(
+            command=str(Path(__file__).parent.parent.parent / '.venv/bin/wikipedia-mcp'),
+            args=[],
+            cwd='/tmp/'
+        )
 
-    def run(self):
+    async def run(self):
         try:
             with langsmith.tracing_context(project_name='hrl/wikisearch', enabled=True, client=self.tracing_client):
                 self.logger.info('Starting Wikisearch agent')
-                prompt = ChatPromptTemplate.from_messages(
-                    [
-                        ('system', 'You are a helpful research librarian with a sense of humor.'),
-                        ('user', 'What is the answer to the ultimate question?')
-                    ]
-                )
-                chain = prompt | self.llm
-                response = chain.invoke({})
-                self.logger.info('OpenAI query', response=response.text())
+                async with stdio_client(self.w_mcp_sever_params) as (w_read, w_write):
+                    async with ClientSession(read_stream=w_read, write_stream=w_write) as w_session:
+                        self.logger.debug('Initilializing MCP server sessions')
+                        await w_session.initialize()
+                        self.logger.debug('MCP session initialized')
+                        tools = await load_mcp_tools(w_session)
+                        self.logger.debug('Got tools', tools=tools)
+                        context = {
+                            'query': 'Mark Twain'
+                        }
+                        prompt = ChatPromptTemplate.from_messages(
+                            [
+                                ('system', 'You are a helpful research librarian with a sense of humor.'),
+                                ('user', 'Ask Wikipedia: what is the birth name of {query}?')
+                            ]
+                        )
+                        # chain = prompt | self.llm.bind_tools(tools=tools) | JsonOutputParser()
+                        # response = await chain.ainvoke(context)
+                        agent = create_react_agent(model=self.llm, tools=tools)
+                        chain = prompt | agent
+                        response = await chain.ainvoke(context)
+                        self.logger.info('OpenAI query', response=response['messages'][-1].content)
         except Exception as e:
             self.logger.exception('Uncaught error somewhere in the code (hopeless).', exc_info=e)
             raise
 
+async def main():
+    app = App()
+    await app.run()
 
 if __name__ == '__main__':
-    App().run()
+    asyncio.run(main=main())
